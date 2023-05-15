87c892456ee92db4aa3714ac63507b2bb08bb9a8
malekinho8 <shmeek8@gmail.com>
2023-05-15 15:36:00 -0400

    Deleted config from genetic algorithm folder

--- genetic-algorithm/config.py
+++
@@ -1,8 +0,0 @@
-ga_settings ={
-    'num_generations': 100,
-    'num_parents_mating': 10,
-    'sol_per_pop': 10,
-    'crossover_type':'uniform',
-    'mutation_type':'random',
-    'mutation_percent_gene':10
-}
